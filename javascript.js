a024bf45e67598aa61b43a9e6bd1f607dbb81a58
Adriana Windmuller <adrianawindmuller@gmail.com>
2020-04-05 16:47:41 +0100

    :sparkles: Concluido card de receitas

--- javascript.js
+++ javascript.js
@@ -1,7 +1,12 @@
 function onLoad() {
     $("#receitaModal").on('show.bs.modal', function () {
+        document.getElementById('img-resultado').src = "";
+        document.getElementById('categoria').value = "Escolha uma categoria";
         document.getElementById('nome-receita').value = "";
-            
+        document.getElementById('tempo-preparo'.value = "");
+        document.getElementById('porcao-receita'.value = "");
+
+
     });
     $("#receitaModal").on('shown.bs.modal', function () {
         document.getElementById('nome-receita').focus();
@@ -11,9 +16,11 @@ function onLoad() {
 var idReceita = 1;
 function inserirReceita() {
     criarCard();
-    criarImagem();
+    criarCardImagem();
+    criarCardCategoria();
     criarCardTitulo();
-
+    criarCardTempo();
+    criarCardPorções();
 
     idReceita++;
 }
@@ -27,7 +34,7 @@ function criarCard() {
     cardDentro.classList.add('card');
 
     let cardBody = document.createElement('div');
-    cardBody.classList.add('card-body', 'text-center');
+    cardBody.classList.add('card-body', 'text-center', 'p-2');
 
     document.getElementById('card-receita').appendChild(cardFora);
 
@@ -36,29 +43,74 @@ function criarCard() {
 
 }
 
-function criarImagem() {
+function criarCardImagem() {
     let cardImagem = document.createElement('img')
     let imagem = document.getElementById('img-resultado').src;
-    
+
     cardImagem.classList.add('card-img-top');
     cardImagem.setAttribute('src', imagem);
     cardImagem.append(imagem);
-    
-    let card =  document.querySelector(`div[id-receita='${idReceita}'] .card`)
+
+    let card = document.querySelector(`div[id-receita='${idReceita}'] .card`)
     card.appendChild(cardImagem);
-    
+
     card.insertBefore(cardImagem, card.childNodes[0]);
 
 }
 
+function criarCardCategoria() {
+    let cardCategoria = document.createElement('p');
+    let categoria = document.getElementById('categoria').value;
+
+    cardCategoria.classList.add('mb-1', 'font-weight-light', 'text-black-50', 'border-bottom', 'border-warning', 'ml-4', 'mr-4');
+    cardCategoria.append(categoria);
+
+    let card = document.querySelector(`div[id-receita='${idReceita}'] > div > .card-body`);
+    card.appendChild(cardCategoria);
+
+    card.insertBefore(cardCategoria, card.childNodes[0]);
+
+}
+
 function criarCardTitulo() {
-    let cardTitulo = document.createElement('h2');
+    let cardTitulo = document.createElement('h4');
     let titulo = document.getElementById('nome-receita').value;
+
+    cardTitulo.classList.add('text-uppercase', 'm-2');
     cardTitulo.append(titulo);
     document.querySelector(`div[id-receita='${idReceita}'] > div > .card-body`).appendChild(cardTitulo)
 
 }
 
+function criarCardTempo() {
+    let tempoPreparo = document.getElementById('tempo-preparo').value;
+    let cardTempo = document.createElement('span');
+    let icone = document.createElement('i');
+
+    tempoPreparo = `${tempoPreparo} Min`; // tempoPrerapo + 'Min'
+
+    icone.classList.add('fas', 'fa-clock', 'text-warning', 'pr-2');
+    cardTempo.append(tempoPreparo);
+    cardTempo.append(icone);
+    document.querySelector(`div[id-receita='${idReceita}'] > div > .card-body`).appendChild(cardTempo);
+
+    cardTempo.insertBefore(icone, cardTempo.childNodes[0]);
+}
+
+function criarCardPorções() {
+    let porcaoReceita = document.getElementById('porcao-receita').value;
+    let cardPorcao = document.createElement('span');
+    let icone = document.createElement('i');
+
+    porcaoReceita = `${porcaoReceita} Porções`; // tempoPrerapo + 'Min'
+
+    icone.classList.add('fas', 'fa-concierge-bell', 'pl-4', 'text-warning', 'pr-2');
+    cardPorcao.append(porcaoReceita);
+    cardPorcao.append(icone);
+    document.querySelector(`div[id-receita='${idReceita}'] > div > .card-body`).appendChild(cardPorcao);
+
+    cardPorcao.insertBefore(icone, cardPorcao.childNodes[0]);
+}
 
 function carregarImagem(event) {
     let input = event.target;
